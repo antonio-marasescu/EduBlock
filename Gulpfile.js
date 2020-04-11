c30500dda2d3ab99123393bf6f81fae22d7c95d6
Marasescu-Duran Antonio <antonio.marasescuduran@gmail.com>
2020-04-11 20:05:50 +0300

    Added graph dependency generation

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,24 @@
 const {exec} = require('child_process');
+const madge = require('madge');
+
 const childNames = ['npm run dev node-uni-1', 'npm run dev node-uni-2', 'npm run dev node-uni-3', 'npm run dev node-uni-4'];
 
+exports.dockerComposeUp = function dockerComposeUp(complete) {
+    const child = exec("docker-compose up", {cwd: 'docker'});
+    child.stdin.pipe(process.stdin);
+    child.stdout.pipe(process.stdout);
+    complete();
+};
+
+exports.dockerComposeDown = function dockerComposeDown(complete) {
+    const child = exec("docker-compose down", {cwd: 'docker'});
+    child.stdin.pipe(process.stdin);
+    child.stdout.pipe(process.stdout);
+    complete();
+};
+
 exports.runNodeA = function runNodeA(complete) {
-    const child = exec(childNames[0],);
+    const child = exec(childNames[0]);
     child.stdin.pipe(process.stdin);
     child.stdout.pipe(process.stdout);
     complete();
@@ -35,5 +51,30 @@ exports.runAllNodes = function runAllNodes(complete) {
         child.stdin.pipe(process.stdin);
         child.stdout.pipe(process.stdout);
     });
+    complete();
+};
+
+
+exports.generateDependencyGraph = function generateDependencyGraph(complete) {
+    madge('app/app.ts')
+        .then((res) => res.image('docs/images/dependency-graph.svg'))
+        .then((writtenImagePath) => {
+            console.log('Image written to ' + writtenImagePath);
+        });
+    complete();
+};
+
+exports.generateDependencyGraphAsDot = function generateDependencyGraphAsDot(complete) {
+    madge('app/app.ts')
+        .then((res) => res.dot())
+        .then((output) => {
+            console.log(output);
+        });
+    complete();
+};
+
+exports.checkCircularDependencies = function checkCircularDependencies(complete) {
+    madge('app/app.ts')
+        .then((res) => console.log(res.circular()));
     complete();
 };
